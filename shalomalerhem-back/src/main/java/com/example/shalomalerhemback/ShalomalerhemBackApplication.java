3c168aad1ba936e61e3927da3c40650b4a3b4ac3
Feideus <erwan.ulrich@kleegroup.com>
2022-03-14 14:41:16 +0100

    First commit

---
+++ shalomalerhem-back/src/main/java/com/example/shalomalerhemback/ShalomalerhemBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.shalomalerhemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShalomalerhemBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShalomalerhemBackApplication.class, args);
+    }
+
+}
---
+++ shalomalerhem-back/src/test/java/com/example/shalomalerhemback/ShalomalerhemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shalomalerhemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShalomalerhemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
